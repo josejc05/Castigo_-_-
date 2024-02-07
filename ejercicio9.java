4027a5deeb5c5e48a5dddede7949a7f09fe2f677
Jose Jurado Cortés <146822057+josejc05@users.noreply.github.com>
2024-02-07 11:39:27 +0000

    .

---
+++ ejercicio9.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la dimension del vector: ");
+        int dimension = scanner.nextInt();
+
+        int[] vector1 = leerVector(scanner, dimension);
+        int[] vector2 = leerVector(scanner, dimension);
+
+        int productoEscalar = calcularProductoEscalar(vector1, vector2);
+
+        System.out.println("El producto escalar es: " + productoEscalar);
+    }
+
+    public static int[] leerVector(Scanner scanner, int dimension) {
+        int[] vector = new int[dimension];
+
+        System.out.println("Ingrese los elementos del vector:");
+
+        for (int i = 0; i < dimension; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            vector[i] = scanner.nextInt();
+        }
+
+        return vector;
+    }
+
+    public static int calcularProductoEscalar(int[] vector1, int[] vector2) {
+        int productoEscalar = 0;
+
+        for (int i = 0; i < vector1.length; i++) {
+            productoEscalar += vector1[i] * vector2[i];
+        }
+
+        return productoEscalar;
+    }
+}
